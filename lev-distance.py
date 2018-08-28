433d5f8eaf4e2ea2c937744cea4647782860526f
pykwon <pykwon@hanmail.net>
2018-08-29 08:48:13 +0900

    Update lev-distance.py

--- lev-distance.py
+++ lev-distance.py
@@ -1,26 +1,32 @@
 # 레벤슈타인 거리 구하기
 
 def calc_distance(a, b):
-    if a == b: return 0    #레벤슈타인 거리 계산하기
+    if a == b: 
+        return 0    # 레벤슈타인 거리 계산
+    
     a_len = len(a)
     b_len = len(b)
     if a == "": return b_len
     if b == "": return a_len
-    # 2차원 표 (a_len+1, b_len+1) 준비하기 --- (※1)
-    matrix = [[] for i in range(a_len+1)]
-    for i in range(a_len+1): # 0으로 초기화
-        matrix[i] = [0 for j in range(b_len+1)]
+    # 2차원 표 (a_len+1, b_len+1) 준비하기 --- 
+    matrix = [[] for i in range(a_len + 1)]
+    
+    for i in range(a_len+1):     # 0으로 초기화
+        matrix[i] = [0 for j in range(b_len + 1)]
+    
     # 0일 때 초깃값을 설정
-    for i in range(a_len+1):
+    for i in range(a_len + 1):
         matrix[i][0] = i
-    for j in range(b_len+1):
+    
+    for j in range(b_len + 1):
         matrix[0][j] = j
-    # 표 채우기 --- (※2)
-    for i in range(1, a_len+1):
+    
+    # 표 채우기 --- 
+    for i in range(1, a_len + 1):
         ac = a[i-1]
         print("ac : ", ac)
-        for j in range(1, b_len+1):
-            bc = b[j-1]
+        for j in range(1, b_len + 1):
+            bc = b[j - 1]
             cost = 0 if (ac == bc) else 1
             matrix[i][j] = min([
                 matrix[i-1][j] + 1,     # 문자 삽입
@@ -29,7 +35,8 @@ def calc_distance(a, b):
             ])
         print("matrix:", matrix)
     return matrix[a_len][b_len]
-# "가나다라"와 "가마바라"의 거리 --- (※3)
+
+# "가나다라"와 "가마바라"의 거리 --- 
 print(calc_distance("가나다라","가마바라"))
     # ac :  가
     # matrix: [[0, 1, 2, 3, 4], [1, 0, 1, 2, 3], [2, 0, 0, 0, 0], [3, 0, 0, 0, 0], [4, 0, 0, 0, 0]]
