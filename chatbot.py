16acded1a9df870c849032125875e121872d4a5d
pykwon <pykwon@hanmail.net>
2018-11-20 14:23:00 +0900

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -7,15 +7,18 @@ form = cgi.FieldStorage()
 # 메인 처리 --- 
 def go_main():
     m = form.getvalue("m", default="")
-    if   m == "" : show_form()
-    elif m == "say" : api_say()
+    if m == "" : 
+        show_form()
+    elif m == "say" : 
+        api_say()
 
 # 사용자의 입력에 응답하기 --- 
 def api_say():
     print("Content-Type: text/plain; charset=utf-8")
     print("")
     txt = form.getvalue("txt", default="")
-    if txt == "": return
+    if txt == "": 
+        return
     res = make_reply(txt)
     print(res)
 
@@ -27,16 +30,18 @@ def show_form():
     <html><meta charset="utf-8"><body>
     <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
     <style>
-        h1   { background-color: #ffe0e0; }
-        div  { padding:10px; }
-        span { border-radius: 10px; background-color: #ffe0e0; padding:8px; }
-        .bot { text-align: left; }
-        .usr { text-align: right; }
+        h1{ background-color: #ffe0e0; }
+        div{ padding:10px; }
+        span{ border-radius: 10px; background-color: #ffe0e0; padding:8px; }
+        .bot{ text-align: left; }
+        .usr{ text-align: right; }
     </style>
-    <h1>대화하기</h1>
+    <h1>* 대화하기 *</h1>
     <div id="chat"></div>
-    <div class='usr'><input id="txt" size="40">
-    <button onclick="say()">전송</button></div>
+    <div class='usr'>
+      <input id="txt" size="40">
+      <button onclick="say()">전송</button>
+    </div>
     <script>
     var url = "./chatbot.py";
     function say() {
@@ -44,15 +49,13 @@ def show_form():
       $.get(url, {"m":"say","txt":txt},
         function(res) {
           var html = "<div class='usr'><span>" + esc(txt) +
-            "</span>: 나</div><div class='bot'> 봇:<span>" + 
-            esc(res) + "</span></div>";
+            "</span>: 나</div><div class='bot'> 봇:<span>" + esc(res) + "</span></div>";
           $('#chat').html($('#chat').html()+html);
           $('#txt').val('').focus();
         });
     }
     function esc(s) {
-        return s.replace('&', '&amp;').replace('<','&lt;')
-                .replace('>', '&gt;');
+        return s.replace('&', '&amp;').replace('<','&lt;').replace('>', '&gt;');
     }
     </script></body></html>
     """)
